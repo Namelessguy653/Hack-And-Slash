7e119ff9cad12c021b90b5019605f458ef935fc7
BrendonPotter <85273403+BrendonPotter@users.noreply.github.com>
2024-04-11 15:35:30 +1000

    fixed walking animations

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -30,25 +30,66 @@ public class CharacterController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical) * speed * Time.deltaTime;
         transform.Translate(movement);
 
-        bool isWalking = Input.GetKey(KeyCode.W);
+        bool isWalkingFoward = Input.GetKey(KeyCode.W);
+        bool isWalkingBackwards = Input.GetKey(KeyCode.S);
+        bool isWalkingRight = Input.GetKey(KeyCode.D);
+        bool isWalkingLeft = Input.GetKey(KeyCode.A);
 
         if (Input.GetKey(KeyCode.W))
         {
 
-            animator.SetBool("IsWalking", isWalking);
+            animator.SetBool("IsWalkingFoward", isWalkingFoward);
+            animator.SetBool("NotWalking", false);
+            animator.SetBool("IsWalkingRight", false);
+            animator.SetBool("IsWalkingLeft", false);
+            animator.SetBool("IsWalkingBackwards", false);
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+
+            animator.SetBool("IsWalkingLeft", isWalkingLeft);
+            animator.SetBool("NotWalking", false);
+            animator.SetBool("IsWalkingRight", false);
+            animator.SetBool("IsWalkingFoward", false);
+            animator.SetBool("IsWalkingBackwards", false);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+            animator.SetBool("IsWalkingRight", isWalkingRight);
+            animator.SetBool("NotWalking", false);
+            animator.SetBool("IsWalkingLeft", false);
+            animator.SetBool("IsWalkingFoward", false);
+            animator.SetBool("IsWalkingBackwards", false);
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+
+            animator.SetBool("IsWalkingBackwards", isWalkingBackwards);
+            animator.SetBool("NotWalking", false);
+            animator.SetBool("IsWalkingRight", false);
+            animator.SetBool("IsWalkingLeft", false);
+            animator.SetBool("IsWalkingFoward", false);
 
         }
         else
         {
-            animator.SetBool("IsWalking", false);
+            animator.SetBool("NotWalking", true);
+            animator.SetBool("IsWalkingRight", false);
+            animator.SetBool("IsWalkingLeft", false);
+            animator.SetBool("IsWalkingFoward", false);
+            animator.SetBool("IsWalkingBackwards", false);
+
         }
 
 
-        // Jump control
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            Debug.Log("space is pressed");
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
+    //    Jump control
+    //    if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+    //    {
+    //        Debug.Log("space is pressed");
+    //        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    //    }
     }
 }
